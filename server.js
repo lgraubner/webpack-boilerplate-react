a94c92100f3e8aed114d1e20a78381f2441defd2
Lars Graubner <mail@larsgraubner.de>
2017-10-27 09:50:58 +0200

    apply prettier formatting

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 /* eslint import/no-extraneous-dependencies:0, global-require:0, no-console:0 */
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const path = require('path')
 
 // create app
-const app = express();
+const app = express()
 
 // set port
-const port = 4080;
-app.set('port', port);
+const port = 4080
+app.set('port', port)
 
 // TODO: SSR
 
 // serve
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')))
 
 app.listen(port, () => {
-  console.log(`Dev server listening on port ${port}.`);
-});
+  console.log(`Dev server listening on port ${port}.`)
+})
--- src/containers/About/__tests__/index.js
+++ src/containers/About/__tests__/index.js
@@ -1,3 +1,3 @@
 test('a simple test', () => {
-  expect(1 + 1).toBe(2);
-});
+  expect(1 + 1).toBe(2)
+})
